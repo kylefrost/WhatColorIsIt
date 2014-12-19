12ab6cb73966ef73a2ab99adef3868355def2c3d
Kyle Frost <kylefrost@comcast.net>
2014-12-19 17:53:05 -0500

    Initial Commit

---
+++ WhatColorIsIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatColorIsIt
+//
+//  Created by Kyle Frost on 12/19/14.
+//  Copyright (c) 2014 Kyle Frost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
