185a006e9174cadf20c6cf1df107b38ad3789fbe
Kyle Frost <kylefrost@comcast.net>
2014-12-19 18:30:10 -0500

    An entire app in 10 minutes don't mind if I do

--- WhatColorIsIt/AppDelegate.swift
+++ WhatColorIsIt/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        // Disabled Screen Idle
+        UIApplication.sharedApplication().idleTimerDisabled = true
+        
         // Override point for customization after application launch.
         return true
     }
--- WhatColorIsIt/ViewController.swift
+++ WhatColorIsIt/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    @IBOutlet var clockLabel: UILabel!
+    @IBOutlet var colorLabel: UILabel!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Add Gesture Recognizer for Taps
+        let recognizer = UITapGestureRecognizer(target: self, action:"copyColor")
+        recognizer.delegate = self
+        view.addGestureRecognizer(recognizer)
+        
+        // Set Clock Label Attrs
+        clockLabel.textColor = UIColor.whiteColor()
+        clockLabel.font = UIFont(name: "OpenSans-Light", size: 80.0)
+        
+        // Set Color Label Attrs
+        colorLabel.textColor = UIColor.whiteColor()
+        colorLabel.font = UIFont(name: "OpenSans-Light", size: 20.0)
+        
+        // Start Clock & Add Timer
+        runClock()
+        NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "runClock", userInfo: nil, repeats: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func runClock() {
+        let date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.CalendarUnitHour | .CalendarUnitMinute | .CalendarUnitSecond, fromDate: date)
+        let currentTime = NSString(format:"%02d : %02d : %02d", components.hour, components.minute, components.second) as String
+        let currentColor = NSString(format:"#%02d%02d%02d", components.hour, components.minute, components.second) as String
+        clockLabel.text = currentTime
+        colorLabel.text = currentColor
+        view.backgroundColor = UIColor(rgba: currentColor)
     }
-
-
+    
+    func copyColor() {
+        println("copyColor()")
+        let color = colorLabel.text
+        UIPasteboard.generalPasteboard().string = color
+    }
+    
+    func gestureRecognizer(UIGestureRecognizer,
+        shouldRecognizeSimultaneouslyWithGestureRecognizer:UIGestureRecognizer) -> Bool {
+            return true
+    }
 }
 
